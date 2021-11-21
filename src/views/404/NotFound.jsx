41d757ec9cf9360e8a58aa113608d2ba4ba0e8ce
mozi <mozi@gmail.com>
2021-11-22 05:29:50 +0800

    update

---
+++ src/views/404/NotFound.jsx
@@ -0,0 +1,19 @@
+import ALaLogo from "../../assets/icons/ala-logo.png";
+import "./notfound.scss";
+import { Trans } from "@lingui/macro";
+
+export default function NotFound() {
+  return (
+    <div id="not-found">
+      <div className="not-found-header">
+        <a href="https://AlaDAO.io" target="_blank">
+          <img className="branding-header-icon" src={ALaLogo} alt="AlaDAO" />
+        </a>
+
+        <h4>
+          <Trans>Page not found</Trans>
+        </h4>
+      </div>
+    </div>
+  );
+}
